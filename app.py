b99e87663d46936afa8d3288d2bb337df23232e7
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-26 15:55:41 +0300

    put secret key in .env and create .env template

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from os import getenv
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+load_dotenv()
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
