4e9abe790135c1abdb60b59c1c2c51da9b1b2c82
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-26 23:32:02 +0300

    restrict un logged in users to view /secrets /download

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from user_table import User, db
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import login_user, LoginManager, login_required, current_user, logout_user
 from os import getenv
 from dotenv import load_dotenv
 
@@ -11,11 +11,17 @@ app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app=app)
-
+login_manager = LoginManager()
+login_manager.init_app(app=app)
 
 # Line below only required once, when creating DB.
 with app.app_context():
     db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 
 @app.route('/')
@@ -33,6 +39,7 @@ def register():
         try:
             db.session.add(user)
             db.session.commit()
+            login_user(user)
         except Exception as e:
             print(e)
             return redirect(url_for('home'))
@@ -40,15 +47,23 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('secrets'))
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    name = request.args.get('name')
-    return render_template("secrets.html", name=name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
@@ -57,6 +72,7 @@ def logout():
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', 'files/cheat_sheet.pdf')
 
